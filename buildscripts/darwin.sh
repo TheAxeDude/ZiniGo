38c2b099ec7b2c9c842867b08bf08ce94c02cdf5
Jason N <99688442+jasonnab@users.noreply.github.com>
2023-02-21 14:17:41 +0000

    Fix typecase
    Won't build unless it's ZiniGo, not Zinigo.

--- buildscripts/darwin.sh
+++ buildscripts/darwin.sh
@@ -1,20 +1,20 @@
 set GOOS=windows
 set GOARCH=amd64
 cd ..
-go build -v -o built/Zinigo_Windows_x64.exe Zinigo/main.go
+go build -v -o built/Zinigo_Windows_x64.exe ZiniGo/main.go
 cd buildscripts
 set GOOS=linux
 set GOARCH=amd64
 cd ..
-go build -o built/Zinigo_Linux_AMD64 Zinigo/main.go
+go build -o built/Zinigo_Linux_AMD64 ZiniGo/main.go
 cd buildscripts
 set GOOS=darwin
 set GOARCH=amd64
 cd ..
-go build -o built/Zinigo_Macos_AMD64 Zinigo/main.go
+go build -o built/Zinigo_Macos_AMD64 ZiniGo/main.go
 cd buildscripts
 set GOOS=windows
 set GOARCH=386
 cd ..
-go build -o built/Zinigo_Windows.exe Zinigo/main.go
+go build -o built/Zinigo_Windows.exe ZiniGo/main.go
 cd buildscripts
